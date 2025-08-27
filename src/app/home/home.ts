d31b0c4a0f7d807057fc0b79b532dde1556a7495
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-27 16:01:02 -0300

    CSS definidos

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css']
 })
 export class Home {
 
