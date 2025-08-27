624987c7c01aa59520c27b600b23201647a5af90
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-27 18:13:34 -0300

    Lógica de artigo

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { articles } from './articles';
+
+// Define a estrutura de um artigo para um código mais seguro
+interface Article {
+  title: string;
+  description: string;
+  source: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -8,6 +17,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class Home {
+export class Home implements OnInit {
+  latestArticle!: Article; // Use "!" para dizer que a variável será inicializada em ngOnInit
 
+  constructor() { }
+
+  ngOnInit(): void {
+    // Verifique se o array de artigos não está vazio antes de atribuir
+    if (articles.length > 0) {
+      this.latestArticle = articles[articles.length - 1];
+    }
+  }
 }
