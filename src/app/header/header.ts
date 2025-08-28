ba8bdd5fa394c7069c30ac29f8765074263b5ed1
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-28 10:57:40 -0300

    updare

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
+  constructor(private router: Router) {}
+
+  isHomePage(): boolean {
+    return this.router.url === '/';
+  }
 
 }
