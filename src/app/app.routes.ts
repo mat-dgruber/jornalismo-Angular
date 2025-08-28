81fa03aaba8c557dcf2a91053956e1fcb875c42c
Matheus Diniz <matheus.gruber123@gmail.com>
2025-08-28 11:21:24 -0300

    Contato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Header } from './header/header';
 import { Artigos } from './artigos/artigos';
 import { Projetos } from './projetos/projetos';
 import { ProjetoTCC } from './projeto-tcc/projeto-tcc';
+import { Contato } from './contato/contato';
 
 
 export const routes: Routes = [
@@ -12,5 +13,6 @@ export const routes: Routes = [
      {path: 'artigos', component: Artigos},
      {path: 'projetos', component: Projetos},
      {path: 'projeto-tcc', component: ProjetoTCC},
+     {path: 'contato', component: Contato},
      {path: '**', redirectTo: ''},
 ];
