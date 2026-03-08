bd04bcef3035fe5b4f9ed57c2b19aa764f75061d
Matheus Diniz <matheus.gruber123@gmail.com>
2026-03-08 10:47:33 -0300

    fix(frontend): update apiUrl to new Google Cloud Run backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   // UPDATE THIS URL AFTER DEPLOYING BACKEND TO RENDER
-  apiUrl: 'https://jornalismo-angular.onrender.com',
+  apiUrl: 'https://backend-373104980698.southamerica-east1.run.app',
   production: true,
   firebase: {
-    apiKey: "AIzaSyCcudQ1-NAZJOG53EWsCfWugj_-PusaH7I",
-    authDomain: "portfolio-jornalismo.firebaseapp.com",
-    projectId: "portfolio-jornalismo",
-    storageBucket: "portfolio-jornalismo.firebasestorage.app",
-    messagingSenderId: "302135985728",
-    appId: "1:302135985728:web:4d3ac269a401149e1581a5",
-    measurementId: "G-JT4VT73HJT"
+    apiKey: 'AIzaSyCcudQ1-NAZJOG53EWsCfWugj_-PusaH7I',
+    authDomain: 'portfolio-jornalismo.firebaseapp.com',
+    projectId: 'portfolio-jornalismo',
+    storageBucket: 'portfolio-jornalismo.firebasestorage.app',
+    messagingSenderId: '302135985728',
+    appId: '1:302135985728:web:4d3ac269a401149e1581a5',
+    measurementId: 'G-JT4VT73HJT',
   },
 };
